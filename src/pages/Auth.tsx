96ed116250e560c3e4faae35f3265f001e8a161b
anand c <148459323+Anandtech09@users.noreply.github.com>
2025-06-23 01:46:24 -0700

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -107,7 +107,7 @@ const Auth: React.FC = () => {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/`
+          redirectTo: 'https://algo-forge-ashy.vercel.app/' 
         }
       });
 
